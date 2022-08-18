09a878eeffb321ec930d752d059e7571730dde52
Artem Potlov <potlov.artem@gmail.com>
2022-08-18 20:56:27 +0300

    feat: add auth render module

---
+++ src/pages/auth-page.ts
@@ -0,0 +1,19 @@
+import authTemplate from '../components/auth/index.hbs';
+import { initAuthController } from '../controllers/auth/auth-controller';
+import { IUserData } from '../types/types';
+import { getLSData } from '../utils/local-storage';
+
+export const initAuth = (element: HTMLElement) => {
+  document.title = 'Авторизация';
+
+  const userData = getLSData<IUserData>('userData');
+  if (userData && userData.token.length > 0) {
+    window.location.replace('');
+  }
+
+  const rootElement = element;
+  rootElement.innerHTML = '';
+  rootElement.innerHTML += authTemplate();
+
+  initAuthController();
+};
