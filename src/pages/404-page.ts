384d2f4ce077b6902f38dd9c0e56e403cd6d1ec9
Artem Potlov <potlov.artem@gmail.com>
2022-08-18 20:55:50 +0300

    feat: add 404 page render module

---
+++ src/pages/404-page.ts
@@ -0,0 +1,9 @@
+import page404Template from '../components/page-404/index.hbs';
+
+export const initPage404 = (element: HTMLElement) => {
+  document.title = 'Страница не найдена';
+
+  const rootElement = element;
+  rootElement.innerHTML = '';
+  rootElement.innerHTML += page404Template();
+};
