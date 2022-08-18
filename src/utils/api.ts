bd2687493839dd2182ff87da6dae9042e1e6221f
Artem Potlov <potlov.artem@gmail.com>
2022-08-18 20:59:17 +0300

    feat: add sigIn and createNewUser functions in api module

---
+++ src/utils/api.ts
@@ -0,0 +1,50 @@
+import { ICreateUserResponse, ISignInResponse, RequestMethod } from '../types/types';
+
+const API_URL = 'https://rslang-team-sixteen.herokuapp.com';
+
+export const signIn = async (email: string, password: string): Promise<ISignInResponse> => {
+  const url = `${API_URL}/signin`;
+  const params: RequestInit = {
+    method: RequestMethod.POST,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  };
+
+  const response = await fetch(url, params);
+
+  if (!response.ok) {
+    return {
+      status: response.status,
+      params: null,
+    };
+  }
+
+  return {
+    status: response.status,
+    params: await response.json(),
+  };
+};
+
+export const createNewUser = async (
+  name: string,
+  email: string,
+  password: string,
+): Promise<ICreateUserResponse> => {
+  const url = `${API_URL}/users`;
+  const params: RequestInit = {
+    method: RequestMethod.POST,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ name, email, password }),
+  };
+
+  const response = await fetch(url, params);
+
+  return {
+    status: response.status,
+    params: await response.json(),
+  };
+};
