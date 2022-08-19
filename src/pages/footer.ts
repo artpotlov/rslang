3e092d5009f5db811129b61737abe9a6724e1c56
Slava-Shchipunov <shchipunovsd@gmail.com>
2022-08-20 02:50:22 +0700

    fix: fix rs_school logo

--- src/pages/footer.ts
+++ src/pages/footer.ts
@@ -1,5 +1,4 @@
 import footer from '../components/main-page/footer.hbs';
-import '../assets/images/svg/rs_school_js.svg';
 
 export const initFooter = (element: HTMLElement) => {
   const rootElement = element;
