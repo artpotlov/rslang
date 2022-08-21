899d3bbe1f0994ac720c60139ae6ae662f94b0d0
Slava-Shchipunov <shchipunovsd@gmail.com>
2022-08-22 02:15:54 +0700

    feat: add main page

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,11 @@ import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
 import { initRouter } from './utils/router';
+import { initMainPage } from './pages/main-page';
 
 const appElement = document.querySelector<HTMLElement>('#app');
 
 if (appElement) {
   initRouter(appElement);
+  initMainPage(appElement);
 }
---
+++ src/pages/main-page.ts
@@ -0,0 +1,21 @@
+import { initEvent } from '../controllers/main-page/main-page-controller';
+
+import header from '../components/main-page/header.hbs';
+import main from '../components/main-page/main.hbs';
+import footer from '../components/main-page/footer.hbs';
+
+export const initMainPage = (element: HTMLElement): void => {
+  const rootElement: HTMLElement = element;
+  const wrapper = `
+    <div class="min-h-full flex flex-col">
+      ${header()}
+      ${main()}
+      ${footer()}
+    </div>`;
+  rootElement.innerHTML += wrapper;
+
+  const headerContainer: HTMLElement | null = document.querySelector('.header');
+  if (!headerContainer) throw new Error('headerContainer is null');
+
+  initEvent(headerContainer);
+};
