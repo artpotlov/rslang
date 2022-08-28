b5b0502e270f1079608147fe8c0bd1b9e4a54199
Aleksander Shustov <krisale@yandex.ru>
2022-08-28 18:32:17 +0300

    refactor: pagination

--- src/controllers/pagination/pagination.ts
+++ src/controllers/pagination/pagination.ts
@@ -1,28 +1,22 @@
 import paginationTemplate from '../../components/pagination/pagination.hbs';
 
-const initPagination = (maxPage: number, currentPage: number, curentGroup?: number) => {
-  const { origin, hash } = window.location;
-  const [location] = hash.split('/').filter((el) => !!el);
+const getLink = (baseUrl: string, page: number, curentGroup?: number) => {
+  return curentGroup !== undefined ? `${baseUrl}${curentGroup}/${page}` : `${baseUrl}/${page}`;
+};
+
+const getPagesData = (
+  maxPage: number,
+  baseUrl: string,
+  currentPage: number,
+  curentGroup?: number,
+) => {
   const defaultVisiblePages = [0, maxPage - 1];
-  const prevLink = {
-    link:
-      curentGroup !== undefined
-        ? `${origin}/${location}/${curentGroup}/${currentPage - 1}`
-        : `${origin}/${location}/${currentPage - 1}`,
-    disabled: currentPage === 0,
-  };
-  const nextLink = {
-    link:
-      curentGroup !== undefined
-        ? `${origin}/${location}/${curentGroup}/${currentPage + 1}`
-        : `${origin}/${location}/${currentPage + 1}`,
-    disabled: currentPage === maxPage - 1,
-  };
   const pages = Array(maxPage)
     .fill('')
     .map((_, index) => {
       const page = index;
       const isCurrent = currentPage === page;
+
       const isVisible =
         defaultVisiblePages.includes(page) ||
         currentPage - 1 === page ||
@@ -30,18 +24,35 @@ const initPagination = (maxPage: number, currentPage: number, curentGroup?: numb
         currentPage - 2 === page ||
         currentPage + 2 === page ||
         isCurrent;
+
       const isEmpty =
         (!defaultVisiblePages.includes(currentPage - 2) && currentPage - 2 === page) ||
         (!defaultVisiblePages.includes(currentPage + 2) && currentPage + 2 === page);
+
       const dataPage = { page: page + 1, link: '', isCurrent, isVisible, isEmpty };
+
       if (isVisible && !isEmpty && !isCurrent) {
-        curentGroup !== undefined
-          ? (dataPage.link = `${origin}/${location}/${curentGroup}/${page}`)
-          : (dataPage.link = `${origin}/${location}/${page}`);
+        dataPage.link = getLink(baseUrl, page, curentGroup);
       }
       return dataPage;
     })
     .filter((el) => el.isVisible);
+  return pages;
+};
+
+const initPagination = (maxPage: number, currentPage: number, curentGroup?: number) => {
+  const { origin, hash } = window.location;
+  const [location] = hash.split('/');
+  const baseUrl = `${origin}/${location}/`;
+  const prevLink = {
+    link: getLink(baseUrl, currentPage - 1, curentGroup),
+    disabled: currentPage === 0,
+  };
+  const nextLink = {
+    link: getLink(baseUrl, currentPage + 1, curentGroup),
+    disabled: currentPage === maxPage - 1,
+  };
+  const pages = getPagesData(maxPage, baseUrl, currentPage, curentGroup);
   return paginationTemplate({
     currentPage,
     pages,
