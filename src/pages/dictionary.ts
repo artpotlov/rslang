4c468cc525307dc763bc46e831aa34f90a2d8d3d
Aleksander Shustov <krisale@yandex.ru>
2022-08-28 22:55:11 +0300

    feat: add header and footer in dictionary

--- src/pages/dictionary.ts
+++ src/pages/dictionary.ts
@@ -1,4 +1,6 @@
 import dictionaryTemplate from '../components/dictionary/index.hbs';
+import headerTemplate from '../components/header/header.hbs';
+import footerTemplate from '../components/footer/footer.hbs';
 import { initDictionaryController } from '../controllers/dictionary/dictionary-controller';
 import { MAX_PAGE_DICTIONARY, TITLES_PAGE } from '../const';
 import setPagination from '../controllers/pagination/setPagination';
@@ -6,7 +8,10 @@ import setPagination from '../controllers/pagination/setPagination';
 export const dictionary = (element: HTMLElement, paramsDictionary = { group: '0', page: '0' }) => {
   document.title = TITLES_PAGE.dictionary;
   const rootElement = element;
-  rootElement.innerHTML = dictionaryTemplate();
+  rootElement.innerHTML = '';
+  rootElement.insertAdjacentHTML('beforeend', headerTemplate());
+  rootElement.insertAdjacentHTML('beforeend', dictionaryTemplate());
+  rootElement.insertAdjacentHTML('beforeend', footerTemplate());
   initDictionaryController(paramsDictionary);
   const dictionaryElement = document.querySelector('[data-role="dictionary"]');
   if (!(dictionaryElement instanceof HTMLElement)) return;
--- src/pages/main-page.ts
+++ src/pages/main-page.ts
@@ -1,9 +1,10 @@
 import '../components/main-page/main-page.scss';
+import './main-page.scss';
 import { initHeaderEvent } from '../controllers/main-page/header-controller';
 import { initMainEvent } from '../controllers/main-page/main-page-controller';
-import header from '../components/main-page/header.hbs';
+import header from '../components/header/header.hbs';
 import main from '../components/main-page/main.hbs';
-import footer from '../components/main-page/footer.hbs';
+import footer from '../components/footer/footer.hbs';
 import team from '../components/main-page/team.hbs';
 
 export const initMainPage = (element: HTMLElement): void => {
