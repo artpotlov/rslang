d487f9a6c347bd4675b74ac04d3f4ab746e0b73c
Aleksander Shustov <krisale@yandex.ru>
2022-09-01 00:59:52 +0300

    feat: add styles of groups

--- src/const.ts
+++ src/const.ts
@@ -27,6 +27,13 @@ export const DINAMIC_CLASSES = {
   cardWordDefault: 'to-blue-200',
   cardWordLerned: 'to-[rgb(16,185,129,.2)]',
   cardWordDifficult: 'to-[rgb(185,16,129,.2)]',
+  colorGroupOne: 'rgba(0,0,0,0.5)',
+  colorGroupTwo: 'rgba(67,56,202,0.5)',
+  colorGroupThree: 'rgba(8,145,178,0.5)',
+  colorGroupFour: 'rgba(101,163,13,0.5)',
+  colorGroupFive: 'rgba(202,138,4,0.5)',
+  colorGroupSix: 'rgba(234,88,12,0.5)',
+  colorGroupSeven: 'rgba(255,28,28,0.5)',
 };
 
 export const MAX_PAGE_DICTIONARY = 30;
--- src/pages/dictionary.ts
+++ src/pages/dictionary.ts
@@ -3,24 +3,35 @@ import headerTemplate from '../components/header/header.hbs';
 import footerTemplate from '../components/footer/footer.hbs';
 import groupButtonsTemplate from '../components/dictionary/groupButtons.hbs';
 import { initDictionaryController } from '../controllers/dictionary/dictionary-controller';
-import { GROUP_DIFFICULT, KEYS_LS, MAX_PAGE_DICTIONARY, TITLES_PAGE } from '../const';
+import {
+  DINAMIC_CLASSES,
+  GROUP_DIFFICULT,
+  KEYS_LS,
+  MAX_PAGE_DICTIONARY,
+  TITLES_PAGE,
+} from '../const';
 import setPagination from '../controllers/pagination/setPagination';
 import { TUserData } from '../types/types';
 import { getLSData } from '../utils/local-storage';
+import getColorGroup from '../helpers/getColorGroup';
 
 export const dictionary = (element: HTMLElement, paramsDictionary = { group: '0', page: '0' }) => {
   document.title = TITLES_PAGE.dictionary;
   const rootElement = element;
+  const { page, group } = paramsDictionary;
+  const colorGroup = getColorGroup(group);
   rootElement.innerHTML = '';
   rootElement.insertAdjacentHTML('beforeend', headerTemplate());
-  rootElement.insertAdjacentHTML('beforeend', dictionaryTemplate());
+  rootElement.insertAdjacentHTML('beforeend', dictionaryTemplate({ colorGroup }));
   rootElement.insertAdjacentHTML('beforeend', footerTemplate());
   initDictionaryController(paramsDictionary);
   const dictionaryElement = document.querySelector('[data-role="dictionary"]');
   if (!(dictionaryElement instanceof HTMLElement)) return;
-  const { page, group } = paramsDictionary;
   const userData: TUserData | null = getLSData(KEYS_LS.userData);
-  dictionaryElement.insertAdjacentHTML('afterbegin', groupButtonsTemplate({ userData }));
+  dictionaryElement.insertAdjacentHTML(
+    'afterbegin',
+    groupButtonsTemplate({ userData, DINAMIC_CLASSES }),
+  );
   if (group !== GROUP_DIFFICULT) {
     setPagination(dictionaryElement, MAX_PAGE_DICTIONARY, +page, +group);
   }
